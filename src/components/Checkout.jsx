aa4d49e8053845b5dae9f34db26aa72836e9a6f2
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-11-10 15:54:51 +0300

    feat: send a post request with order data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -18,7 +18,19 @@ export default function Checkout() {
     event.preventDefault();
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
-    console.log(customerData);
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -26,7 +38,7 @@ export default function Checkout() {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label='Full Name' type='text' id='full-name' />
+        <Input label='Full Name' type='text' id='name' />
         <Input label='E-Mail Adress' type='email' id='email' />
         <Input label='Street' type='text' id='street' />
         <div className='control-row'>
